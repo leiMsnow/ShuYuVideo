d0eb404d3506968e3f7e49c94b696148dd02a5e8
ray <always_0122@163.com>
2016-08-29 21:31:34 +0800

    Initial commit

---
+++ corelibrary/src/main/java/com/shuyu/core/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.shuyu.core;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Azure on 2016/8/29.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+}
