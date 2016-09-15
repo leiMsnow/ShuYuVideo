9bdd17df819172710a4c8fb1fda9f466ad214b6d
ray <always_0122@163.com>
2016-09-15 12:54:41 +0800

    修改VIP页

--- app/src/main/java/com/shuyu/video/adapter/VipPageAdapter.java
+++ app/src/main/java/com/shuyu/video/adapter/VipPageAdapter.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.shuyu.video.R;
@@ -63,8 +63,9 @@ public class VipPageAdapter extends PagerAdapter {
         if (mConvertViews.size() == 0) {
             holder = new ViewHolder();
             convertView = View.inflate(mContext, R.layout.item_vip_page, null);
+            holder.cvVip = convertView.findViewById(R.id.cv_vip);
             holder.ivUrl = (ImageView) convertView.findViewById(R.id.iv_url);
-            holder.btnVideo = (Button) convertView.findViewById(R.id.btn_vip_video);
+            holder.btnVideo = (TextView) convertView.findViewById(R.id.tv_video_title);
             convertView.setTag(holder);
         } else {
             convertView = mConvertViews.removeFirst();
@@ -73,15 +74,16 @@ public class VipPageAdapter extends PagerAdapter {
 
         Glide.with(mContext).load(mLiveVideoDataList.get(position).getImgUrl()).into(holder.ivUrl);
         holder.btnVideo.setText(mLiveVideoDataList.get(position).getTitle());
-        holder.btnVideo.setTag(mLiveVideoDataList.get(position));
-        holder.btnVideo.setOnClickListener(mOnClickListener);
+        holder.cvVip.setTag(mLiveVideoDataList.get(position));
+        holder.cvVip.setOnClickListener(mOnClickListener);
         container.addView(convertView);
         return convertView;
     }
 
     private class ViewHolder {
+        View cvVip;
         ImageView ivUrl;
-        Button btnVideo;
+        TextView btnVideo;
     }
 
     @Override
