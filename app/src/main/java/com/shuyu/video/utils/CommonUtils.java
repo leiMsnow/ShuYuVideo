2665728e858c00734d8a42f39b70e6bda2881b9d
ray <always_0122@163.com>
2016-11-11 23:08:52 +0800

    xiugai IMEI

--- app/src/main/java/com/shuyu/video/utils/CommonUtils.java
+++ app/src/main/java/com/shuyu/video/utils/CommonUtils.java
@@ -15,8 +15,8 @@ import java.util.Map;
 public class CommonUtils {
 
     public static String getIMSI() {
-        return "460027588739342";
-//        return AppUtils.getIMSI();
+//        return "460027588739342";
+        return AppUtils.getIMSI();
     }
 
     public static String getIMEI() {
